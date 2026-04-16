eea105b39a40051622840c9cb4a5af08dabfb9e1
wx4812 <3485341992@qq.com>
2026-04-16 12:33:34 +0900

    Delete build directory

--- build/ros2_kitti_publishers/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lixi/ros2_ws/src/ros2_kitti_publishers
-# Build directory: /home/lixi/ros2_ws/build/ros2_kitti_publishers
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
