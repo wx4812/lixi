ef9db888e865866209585cc1015f54fc76e525c0
wx4812 <3485341992@qq.com>
2026-04-16 12:32:23 +0900

    整理ROS2文件到week1文件夹

---
+++ build/ros2_kitti_publishers/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/lixi/ros2_ws/src/ros2_kitti_publishers
+# Build directory: /home/lixi/ros2_ws/build/ros2_kitti_publishers
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
